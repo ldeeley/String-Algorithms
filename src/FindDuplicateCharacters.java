1b5cf1d813d3a0116c693e5d9c6cbece25604013
Lester Deeley <lester.deeley@yahoo.com>
2020-08-20 14:49:25 +0100

    Initial commit

--- src/FindDuplicateCharacters.java
+++ src/FindDuplicateCharacters.java
@@ -1,2 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class FindDuplicateCharacters {
+
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+
+        String word = reader.readLine();
+        displayDuplicateCharacters(word);
+
+    }
+
+    public static void displayDuplicateCharacters(String word) {
+        char[] characters = word.toCharArray();
+
+        Map<Character, Integer> charMap = new HashMap<>();
+        for (Character ch : characters) {
+            if (charMap.containsKey(ch)) {
+                charMap.put(ch, charMap.get(ch) + 1);
+            } else charMap.put(ch, 1);
+        }
+
+        Set<Map.Entry<Character, Integer>> entrySet = charMap.entrySet();
+        System.out.printf("List of duplicate characters in String '%s' %n", word);
+        for (Map.Entry<Character, Integer> entry : entrySet) {
+            if (entry.getValue() > 1) {
+                System.out.printf("%s : %d %n", entry.getKey(), entry.getValue());
+            }
+        }
+    }
+
 }
